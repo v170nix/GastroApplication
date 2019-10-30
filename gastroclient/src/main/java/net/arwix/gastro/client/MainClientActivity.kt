c0fd334a8e3fd9d72af56df28192c909dde3a6cc
Vitaliy Sheyanov <vit.onix@gmail.com>
2019-10-30 23:27:47 +0300

    Initial commit

---
+++ gastroclient/src/main/java/net/arwix/gastro/client/MainClientActivity.kt
@@ -0,0 +1,12 @@
+package net.arwix.gastro.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainClientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_client)
+    }
+}
