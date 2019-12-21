179183f00830c77e249d0c86b53e03bcf4957e64
Vitaliy Sheyanov <vit.onix@gmail.com>
2019-12-21 22:31:16 +0300

    change task

--- library/src/main/java/net/arwix/gastro/library/TaskExtension.kt
+++ library/src/main/java/net/arwix/gastro/library/TaskExtension.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
+// https://github.com/EddyVerbruggen/nativescript-plugin-firebase/issues/631
 suspend fun <T> Task<T>.await(): T? {
     if (isComplete) {
         val e = exception
@@ -17,13 +18,33 @@ suspend fun <T> Task<T>.await(): T? {
     }
 
     return suspendCancellableCoroutine { cont ->
-        addOnCompleteListener {
-            val e = exception
-            if (e == null) {
-                if (isCanceled) cont.cancel() else cont.resume(result)
-            } else {
-                cont.resumeWithException(e)
-            }
+        addOnSuccessListener {
+            if (isCanceled) cont.cancel() else cont.resume(it)
+        }
+        addOnFailureListener {
+            cont.resumeWithException(it)
         }
     }
 }
+
+//suspend fun <T> Task<T>.await(): T? {
+//    if (isComplete) {
+//        val e = exception
+//        return if (e == null) {
+//            if (isCanceled)
+//                throw CancellationException("Task $this was cancelled normally")
+//            else result
+//        } else throw e
+//    }
+//
+//    return suspendCancellableCoroutine { cont ->
+//        addOnCompleteListener {
+//            val e = exception
+//            if (e == null) {
+//                if (isCanceled) cont.cancel() else cont.resume(result)
+//            } else {
+//                cont.resumeWithException(e)
+//            }
+//        }
+//    }
+//}
